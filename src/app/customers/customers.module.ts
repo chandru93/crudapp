a52b7b2d2581b4d0adef4f88dc69d863d7d9fead
chandru <chandruu.ragu@gmail.com>
2020-02-25 23:24:03 +0530

    First commit

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { EffectsModule, Actions } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+
+import { customerReducer } from "./state/customer.reducer";
+import { CustomerEffect } from "./state/customer.effects";
+
+import { CustomerComponent } from "./customer/customer.component";
+import { CustomerAddComponent } from "./customer-add/customer-add.component";
+import { CustomerEditComponent } from "./customer-edit/customer-edit.component";
+import { CustomerListComponent } from "./customer-list/customer-list.component";
+
+const customerRoutes: Routes = [{ path: "", component: CustomerComponent }];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FontAwesomeModule,
+    RouterModule.forChild(customerRoutes),
+    StoreModule.forFeature("customers", customerReducer),
+    EffectsModule.forFeature([CustomerEffect])
+  ],
+  declarations: [
+    CustomerComponent,
+    CustomerAddComponent,
+    CustomerEditComponent,
+    CustomerListComponent
+  ]
+})
+export class CustomersModule {}
